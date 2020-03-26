ab6111f18b185cd0a726c6d0682713bb751deeea
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-03-26 14:07:59 -0700

    Fix build, tests, and codecov token

--- src/MoistThermodynamics.jl
+++ src/MoistThermodynamics.jl
@@ -8,17 +8,14 @@ saturation specific humidities.
 module MoistThermodynamics
 
 using DocStringExtensions
+using RootSolvers
+using CLIMAParameters
+using CLIMAParameters.Planet
 
 @inline q_pt_0(::Type{FT}) where {FT} = PhasePartition{FT}(FT(0), FT(0), FT(0))
-
-using RootSolvers
-
-include("Parameters.jl")
-using .Parameters
 
 include("states.jl")
 include("isentropic.jl")
 include("relations.jl")
-include("test_moist_thermo.jl")
 
 end
