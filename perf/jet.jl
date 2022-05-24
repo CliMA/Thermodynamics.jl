b0319f974089b014270b0c76dc873c39e519d03b
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-05-23 19:36:27 -0700

    Extend JET tests

--- perf/jet.jl
+++ perf/jet.jl
@@ -11,9 +11,13 @@ function jet_thermo_states()
     @testset "JET tests" begin
         for C in (
             TD.PhaseEquil_ρeq,
-            TD.PhaseEquil_peq,
-            TD.PhaseEquil_pθq,
+            TD.PhaseEquil_ρTq,
             TD.PhaseEquil_pTq,
+            TD.PhaseEquil_peq,
+            TD.PhaseEquil_phq,
+            TD.PhaseEquil_ρθq,
+            TD.PhaseEquil_pθq,
+            TD.PhaseEquil_ρpq,
         )
             for cond in conditions(C)
                 args = sample_args(profiles, cond, C)
