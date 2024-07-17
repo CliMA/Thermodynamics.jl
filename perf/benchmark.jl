b335c7a1b65d05b15c01a37d3192a7583535c7b7
Charles Kawczynski <kawczynski.charles@gmail.com>
2024-07-17 14:26:34 -0400

    Refactor allocation monitoring
    Update Project tomls

--- perf/benchmark.jl
+++ perf/benchmark.jl
@@ -2,11 +2,22 @@ include("common.jl")
 
 @testset "Thermodynamics - Performance pθq constructor" begin
 
-    trial = BenchmarkTools.@benchmark $thermo_state_ρeq()
+    trial = BenchmarkTools.@benchmark thermo_state_ρeq(
+        $param_set,
+        $ρ,
+        $e_int,
+        $q_tot,
+    )
     show(stdout, MIME("text/plain"), trial)
-    trial = BenchmarkTools.@benchmark $thermo_state_pθq()
+    trial = BenchmarkTools.@benchmark thermo_state_pθq(
+        $param_set,
+        $p,
+        $θ_liq_ice,
+        $q_tot,
+    )
     show(stdout, MIME("text/plain"), trial)
-    trial = BenchmarkTools.@benchmark $thermo_state_pTq()
+    trial =
+        BenchmarkTools.@benchmark thermo_state_pTq($param_set, $p, $T, $q_tot)
     show(stdout, MIME("text/plain"), trial)
 
 end
--- perf/common.jl
+++ perf/common.jl
@@ -10,18 +10,30 @@ import ClimaParams as CP
 const FT = Float64
 const param_set = TP.ThermodynamicsParameters(FT)
 
+function thermo_state_ρeq(param_set, _ρ, _e_int, _q_tot)
+    for i in 1:length(_q_tot)
+        (ρ, e_int, q_tot) = (_ρ[i], _e_int[i], _q_tot[i])
+        ts = TD.PhaseEquil_ρeq(param_set, ρ, e_int, q_tot)
+    end
+    return nothing
+end
+
+function thermo_state_pθq(param_set, _p, _θ_liq_ice, _q_tot)
+    for i in 1:length(_q_tot)
+        (p, θ_liq_ice, q_tot) = (_p[i], _θ_liq_ice[i], _q_tot[i])
+        ts = TD.PhaseEquil_pθq(param_set, p, θ_liq_ice, q_tot)
+    end
+    return nothing
+end
+
+function thermo_state_pTq(param_set, _p, _T, _q_tot)
+    for i in 1:length(_q_tot)
+        (p, T, q_tot) = (_p[i], _T[i], _q_tot[i])
+        ts = TD.PhaseEquil_pTq(param_set, p, T, q_tot)
+    end
+    return nothing
+end
+
 ArrayType = Array{FT}
 profiles = TD.TestedProfiles.PhaseEquilProfiles(param_set, ArrayType)
 (; e_int, T, ρ, p, θ_liq_ice, q_tot) = profiles
-
-function thermo_state_ρeq()
-    ts = TD.PhaseEquil_ρeq.(param_set, ρ, e_int, q_tot)
-end
-
-function thermo_state_pθq()
-    ts = TD.PhaseEquil_pθq.(param_set, p, θ_liq_ice, q_tot)
-end
-
-function thermo_state_pTq()
-    ts = TD.PhaseEquil_pTq.(param_set, p, T, q_tot)
-end
