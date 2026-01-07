cfcf9d962eb34e56d5b378909dba59022c49485a
Tapio Schneider <tapio@caltech.edu>
2026-01-07 00:10:23 -0800

    Remove warnings

--- src/printing.jl
+++ src/printing.jl
@@ -26,36 +26,12 @@ for rsm in (
         ),
     )
         @eval function print_warning(::Type{<:$(rsm)}, ::$IV, args...)
-            ($(args...),) = args
-            $(
-                let
-                    call_args = Any[]
-                    push!(call_args, "maxiter reached in saturation_adjustment($name):\n")
-                    push!(call_args, string(rsm))
-                    push!(call_args, "(")
-                    for (i, a) in enumerate(args)
-                        push!(call_args, "$(string(a))=")
-                        push!(call_args, a) # Symbol representing variable
-                        if i < length(args)
-                            push!(call_args, ", ")
-                        end
-                    end
-                    push!(call_args, ")" * (name == "ρe" ? "" : " ="))
-                    push!(call_args, "\n")
-                    Expr(:macrocall, GlobalRef(KA, Symbol("@print")), LineNumberNode(@__LINE__), call_args...)
-                end
-            )
+            return nothing
         end
     end
 
     # Deprecated function, to be removed:
     @eval function print_warning_ρθq_nonlinear(::Type{<:$(rsm)}, args...)
-        (θ_liq_ice, ρ, q_tot, q_liq, q_ice, T, maxiter, tol) = args
-        return KA.@print(
-            "maxiter reached in saturation_adjustment(ρθq_nonlinear):\n",
-            $(string(rsm)),
-            "(θ_liq_ice=", θ_liq_ice, ", ρ=", ρ, ", q_tot=", q_tot, ", q_liq=", q_liq, ", q_ice=", q_ice, ", T=", T, ", maxiter=", maxiter, ", tol=", tol, ") =",
-            "\n"
-        )
+        return nothing
     end
 end
